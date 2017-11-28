d1d95f040cd5f11dfbfcb1d86d32b56b8bd8078a
Joshua Russell <jkrussell756@gmail.com>
2017-11-28 17:49:57 -0500

    Racers move with the correct key press

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 // grabbing info from the DOM
 const racer1 = document.getElementById("racer1");
-// add an event listener for that element
-document.addEventListener('keydown.',moveRacer1);
 // define the racer2 variable
 const racer2 = document.getElementById("racer2");
-// add an event listener for racer2
-document.addEventListener('keydown.',moveRacer2);
+// add an event listener for the moveRacers function
+document.addEventListener('keydown',moveRacers);
 
-
-
-
-// helper functions
+// Starting point for each racer
 let racer1Val = 140;
-function moveRacer1(event) {
-  console.log(event)
+let racer2Val = 140;
+
+function moveRacers(event) {
+  if (event.keyCode == 39) {
+      console.log(event)
   racer1Val = racer1Val + 100;
   racer1.style.left = racer1Val + "px";
   console.log(racer1.style.left)
   checkForMatch()
-}
-
-// moving racer2 function
-
-let racer2Val = 140;
-function moveRacer2(event) {
-  console.log(event)
+  } if (event.keyCode === 90) {
+      console.log(event)
   racer2Val = racer2Val + 100;
   racer2.style.left = racer2Val + "px";
   console.log(racer2.style.left)
   checkForMatch()
+  }
 }
