9b3ab1cd52c0bb7057d5c3157c8c6287a2eacdde
Joshua Russell <jkrussell756@gmail.com>
2017-11-28 17:11:04 -0500

    Added some js functionality

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 // grabbing info from the DOM
 const racer1 = document.getElementById("racer1");
 // add an event listener for that element
-racer1.addEventListener("click",moveRacer1);
-// document.addEventListener('keydown',moveRacer1)
+document.addEventListener('keydown.',moveRacer1);
+// define the racer2 variable
 const racer2 = document.getElementById("racer2");
+// add an event listener for racer2
+document.addEventListener('keydown.',moveRacer2);
+
+
 
 
 // helper functions
-
-let racer1Val = 0;
-function moveRacer1() {
+let racer1Val = 140;
+function moveRacer1(event) {
+  console.log(event)
   racer1Val = racer1Val + 100;
   racer1.style.left = racer1Val + "px";
   console.log(racer1.style.left)
-  checkformatch();
-};
+  checkForMatch()
+}
+
+// moving racer2 function
+
+let racer2Val = 140;
+function moveRacer2(event) {
+  console.log(event)
+  racer2Val = racer2Val + 100;
+  racer2.style.left = racer2Val + "px";
+  console.log(racer2.style.left)
+  checkForMatch()
+}
