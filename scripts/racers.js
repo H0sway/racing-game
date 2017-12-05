d7f50931d99a8d176a0662de013b16cf8527a0d5
Joshua Russell <jkrussell756@gmail.com>
2017-12-04 22:34:32 -0500

    Working using JQuery

---
+++ scripts/racers.js
@@ -0,0 +1,35 @@
+$(document).ready(() => {
+  // Grab the racers
+  let $racer1 = $("#racer1");
+  let $racer2 = $("#racer2");
+  // Used for checking the winner
+  let racer1Val = 0;
+  let racer2Val = 0;
+  // Grab the reset button
+  let $button = $("#button");
+
+  $(document).keyup(move);
+
+  function move() {
+    if (racer1Val === 36) {
+      return alert("Plane Wins!")
+    } if (racer2Val === 36) {
+      return alert("Car Wins");
+    } if (event.which === 39) {
+      racer1Val = racer1Val + 1;
+      $racer1.css({left: "+=25px"});
+    } if (event.which === 90) {
+      racer2Val = racer2Val + 1;
+      $racer2.css({left: "+=25px"});
+    }
+  }
+
+
+
+$button.click(() => {
+  racer1Val = 0;
+  racer2Val = 0;
+  $racer1.css("left","5px");
+  $racer2.css("left","5px");
+  });
+}); // End of document.ready
