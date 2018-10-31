1a45e6c06a60a2316a863364a58f33455e452d33
Joshua Russell <jkrussell756@gmail.com>
2018-10-31 09:02:29 -0400

    modernizing the JS, bringing it more up to date.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,15 @@
 $(document).ready(() => {
-  console.log("Hi mom!");
+  console.log("jQuery is online");
   const $button = $("#button");
   $(document).keyup(move);
 
   class Racer {
-    constructor(name,id,keyCode,moves) {
+    constructor(name, id, keyCode, moves, wins) {
       this.name = name;
       this.id = $(id);
       this.keyCode = keyCode;
       this.moves = moves;
+      this.wins = wins;
     }
   }
 
@@ -16,50 +17,65 @@ $(document).ready(() => {
     name: "Plane",
     id: "#racer1",
     keyCode: 39,
-    moves: 0
-  }
-  let car = {
-    name: "Car",
-    id: "#car",
-    keyCode: 90,
     moves: 0,
+    wins: 0
   }
 
+  let car = {
+    name: "Car",
+    id: "#racer2",
+    keyCode: 90,
+    moves: 0,
+    wins: 0
+  }
+
+  const reset = () => {
+    plane.moves = 0;
+    car.moves = 0;
+    $(plane.id).css("left","5px");
+    $(car.id).css("left","5px");
+  };
+
   function move() {
-    if (plane.moves === 36) {
-      return alert("Plane Wins!")
-    } if (car.moves === 36) {
-      return alert("Car Wins");
-    } if (event.which === plane.keyCode) {
+    if (plane.moves === 35) {
+      alert("Plane Wins!");
+      reset();
+      return
+    }
+    if (car.moves === 35) {
+      alert("Car Wins");
+      reset();
+      return
+    }
+    if (event.which === plane.keyCode) {
       plane.moves = plane.moves + 1;
-      $("#racer1").css({left: "+=25px"});
-    } if (event.which === car.keyCode) {
+      $(plane.id).css({left: "+=25px"});
+    }
+    if (event.which === car.keyCode) {
       car.moves = car.moves + 1;
-      $("#racer2").css({left: "+=25px"});
+      $(car.id).css({left: "+=25px"});
     }
   }
 
   $button.click(() => {
-  plane.moves = 0;
-  car.moves = 0;
-  $("#racer1").css("left","5px");
-  $("#racer2").css("left","5px");
+    reset();
   });
 
-  const $form = $("form");
-  $form.submit(answer);
-
-  function answer(event) {
+  const answer = (event) => {
+    event.preventDefault();
     const $select = $("select[name=answer]");
     const $first = $("#first");
     const $second = $("#second");
-    event.preventDefault();
-    if ($select.val("Yes")) {
+    if ($select.val($first)) {
       console.log("Yes");
       return alert("Thank you very much!");
-    } if ($select.val("No")) {
+    }
+    if ($select.val($second)) {
       console.log("No");
       return alert("Well, it don't like you much either");
     }
-  }
+  };
+
+  const $form = $("form");
+  $form.submit(answer);
 }) // end of document.ready
